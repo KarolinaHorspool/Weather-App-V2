38ea7b0f101d25c67584829f663dba1dd6044d02
KarolinaHorspool <karolina.horspool@gmail.com>
2020-12-07 20:50:01 +0000

    function hours

--- src/script.js
+++ src/script.js
@@ -10,6 +10,19 @@ function display_c() {
             }
 let now = new Date();
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hour}:${minutes}`;
+}
+
 function displayWeather(response) {
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
